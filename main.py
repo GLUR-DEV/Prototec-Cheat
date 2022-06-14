d19496d269d2e1591690e2064d47282502d7de8c
GLUR-DEV <82645786+GLUR-DEV@users.noreply.github.com>
2022-06-14 18:16:26 +1200

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,18 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
+substring0 = "Thousandths"
+substring1 = "Hundredths"
+substring2 = "Tenths"
+
+i = 1
+placevaltype = 0
+offset = 0
+
 sup = []
 sub = []
+frac2dec = []
+fracresult = []
 
 options = Options()
 # options.add_argument("--window-size=1920,1080")
@@ -19,92 +29,59 @@ driver.get("https://maths.prototec.co.nz/")
 folder = driver.find_element_by_xpath("//button[@value='s8']")
 folder.click()
 
-folder = driver.find_element_by_xpath("(//sup)[1]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[2]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[3]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[4]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[5]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[6]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[7]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[8]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[9]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[10]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[11]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[12]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[13]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[14]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[15]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[16]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[17]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[18]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[19]").text
-sup.append(folder)
-folder = driver.find_element_by_xpath("(//sup)[20]").text
-sup.append(folder)
+while i <= 20:
+    folder = driver.find_element_by_xpath("(//sup)[" + str(i) + "]").text
+    sup.append(folder)
+    i = i + 1
 
-folder = driver.find_element_by_xpath("(//sub)[1]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[2]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[3]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[4]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[5]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[6]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[7]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[8]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[9]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[10]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[11]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[12]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[13]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[14]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[15]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[16]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[17]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[18]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[19]").text
-sub.append(folder)
-folder = driver.find_element_by_xpath("(//sub)[20]").text
-sub.append(folder)
+i = 1
 
+while i <= 20:
+    folder = driver.find_element_by_xpath("(//sub)[" + str(i) + "]").text
+    sub.append(folder)
+    i = i + 1
 
+i = 0
 
-#print(sup)
-#print(sub)
+while i <= 20:
+    folder = driver.find_element_by_xpath("(//td)[" + i + offset + 35 + "]").text
+
+print(sup)
+print(sub)
+
+while i < 20:
+    frac2dec.append(int(sup[i]) / int(sub[i]))
+    i = i + 1
+
+print(frac2dec)
+i = 0
+offset = 0
+
+while i < 10:
+   folder = driver.find_element_by_xpath("//input[@name='q" + str(i) + "']")
+
+   if frac2dec[i+offset] > frac2dec[i+offset+1]:
+       folder.click()
+       folder.send_keys(">")
+   elif frac2dec[i+offset] < frac2dec[i+offset+1]:
+       folder.click()
+       folder.send_keys("<")
+   elif frac2dec[i+offset] == frac2dec[i+offset+1]:
+       folder.click()
+       folder.send_keys("=")
+
+   i = i + 1
+   offset = offset + 1
+
+i = 0
+
+while i < 10:
+    if substring0 in
+
 sleep(100)
+
+
+
+
 
 
